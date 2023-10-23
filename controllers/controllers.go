a1face328dd3fe5727f0d2aca18bcd7f5f97dcba
Kuthumi Pepple <kuthumipepple@gmail.com>
2023-10-23 15:58:00 +0100

    define http routes

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,23 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func AddProduct() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func GetProducts() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func SearchProductByQuery() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kuthumipepple/ecommerce-platform/controllers"
+)
+
+func UserRoutes(routes *gin.Engine) {
+	routes.POST("/users/signup", controllers.Signup())
+	routes.POST("/users/login", controllers.Login())
+	routes.POST("/admin/products", controllers.AddProduct())
+	routes.GET("/users/products", controllers.GetProducts())
+	routes.GET("/users/search", controllers.SearchProductByQuery())
+}
