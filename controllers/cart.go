ec57c7a8eb56104927b593ad0b427bbd087a8455
Kuthumi Pepple <kuthumipepple@gmail.com>
2023-10-25 22:43:54 +0100

    add main fuction to start server

--- controllers/cart.go
+++ controllers/cart.go
@@ -1,1 +1,15 @@
 package controllers
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+type Application struct {
+	productsCollection *mongo.Collection
+	usersCollection    *mongo.Collection
+}
+
+func NewApplication(productsCollectionName, usersCollectionName *mongo.Collection) *Application {
+	return &Application{
+		productsCollection: productsCollectionName,
+		usersCollection:    usersCollectionName,
+	}
+}
--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kuthumipepple/ecommerce-platform/controllers"
+	"github.com/kuthumipepple/ecommerce-platform/database"
+	"github.com/kuthumipepple/ecommerce-platform/routes"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	controllers.NewApplication(
+		database.OpenCollection("products"),
+		database.OpenCollection("users"),
+	)
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	log.Fatal(router.Run(":" + port))
+}
