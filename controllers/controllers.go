ba059921f5e74e4b1666486664f7626ae0af0f13
Kuthumi Pepple <kuthumipepple@gmail.com>
2023-10-30 10:15:57 +0100

    feature: handle request to signup

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,9 +1,94 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/kuthumipepple/ecommerce-platform/database"
+	"github.com/kuthumipepple/ecommerce-platform/models"
+	"github.com/kuthumipepple/ecommerce-platform/tokens"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var userCollection = database.OpenCollection("users")
+
+func HashPassword(passwd string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(passwd), 12)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(hash)
+}
 
 func Signup() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		newUser := models.User{}
+		if err := c.BindJSON(&newUser); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if validationErr := validator.New().Struct(newUser); validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": newUser.Email})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			log.Panic(err)
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user with this email already exists"})
+			return
+		}
+
+		count, err = userCollection.CountDocuments(ctx, bson.M{"phone": newUser.Phone})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			log.Panic(err)
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "this phone numbeer is already in use"})
+			return
+		}
+
+		password := HashPassword(*newUser.Password)
+
+		newUser.Password = &password
+		newUser.CreatedAt = time.Now().Local()
+		newUser.UpdatedAt = time.Now().Local()
+		newUser.ID = primitive.NewObjectID()
+		newUser.UserID = newUser.ID.Hex()
+
+		token, refreshToken, err := tokens.GenerateTokens(*newUser.Email, *newUser.FirstName, *newUser.LastName, newUser.UserID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error while generating tokens"})
+		}
+
+		newUser.Token = &token
+		newUser.RefreshToken = &refreshToken
+		newUser.UserCart = make([]models.UserProduct, 0)
+		newUser.AddressDetails = make([]models.Address, 0)
+		newUser.OrderStatus = make([]models.Order, 0)
+
+		insertionResult, insertionErr := userCollection.InsertOne(ctx, newUser)
+		if insertionErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "the user did not get created"})
+			return
+		}
+
+		c.JSON(http.StatusCreated, insertionResult)
+	}
 }
 
 func Login() gin.HandlerFunc {
--- tokens/token_gen.go
+++ tokens/token_gen.go
@@ -1,1 +1,48 @@
 package tokens
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type SignedDetails struct {
+	Email     string
+	FirstName string
+	LastName  string
+	UserID    string
+	jwt.RegisteredClaims
+}
+
+var SECRET_KEY = os.Getenv("JWT_SECRET_KEY")
+
+func GenerateTokens(email, firstName, lastName, userID string) (signedToken, signedRefreshToken string, err error) {
+	claims := SignedDetails{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+		UserID:    userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Local().Add(time.Duration(24) * time.Hour)),
+		},
+	}
+
+	refreshClaims := SignedDetails{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Local().Add(time.Duration(168) * time.Hour)),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", "", err
+	}
+
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", "", err
+	}
+
+	return token, refreshToken, err
+}
